b3073b6423e805304b26c5bd00060003c96d6319
marcia <marciatainan@hotmail.com>
2019-12-17 13:24:08 -0300

    Fazendo rodas

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -4,34 +4,34 @@ const controller = require("../controllers/adminController")
 const jwt = require('jsonwebtoken')
 const SEGREDO = process.env.SEGREDO
 
-// const autenticar = (request, response, next) => {
-//   const authHeader = request.get('authorization')
-//   let autenticado = false
+const autenticar = (request, response, next) => {
+  const authHeader = request.get('authorization')
+  let autenticado = false
 
-//   if (!authHeader) {
-//     return response.status(401).send('Você precisa fazer login!')
-//   }
+  if (!authHeader) {
+    return response.status(401).send('Você precisa fazer login!')
+  }
 
-//   const token = authHeader.split(' ')[1]
+  const token = authHeader.split(' ')[1]
 
-//   jwt.verify(token, SEGREDO, (error, decoded) => {
-//     if (error) {
-//       autenticado = false
-//     } else {
-//       if (decoded.grupo === 'comum' || decoded.grupo === 'admin') {
-//         autenticado = true
-//       } else {
-//         autenticado = false
-//       }
-//     }
-//   })
+  jwt.verify(token, SEGREDO, (error, decoded) => {
+    if (error) {
+      autenticado = false
+    } else {
+      if (decoded.grupo === 'comum' || decoded.grupo === 'admin') {
+        autenticado = true
+      } else {
+        autenticado = false
+      }
+    }
+  })
 
-//   if (!autenticado) {
-//     return response.status(403).send('Acesso negado.')
-//   }
+  if (!autenticado) {
+    return response.status(403).send('Acesso negado.')
+  }
 
-//   next()
-// }
+  next()
+}
   
 
 const autenticarAdmin = (request, response, next) => {
@@ -67,6 +67,6 @@ router.post('', controller.addAdmin)
 router.delete('/:id', autenticarAdmin, controller.remove)
 router.patch ('/:id', autenticarAdmin, controller.update)
 router.post('/login', controller.login)
-// router.post('/aluno',autenticarAdmin, controller.addStudent)
+router.post('/aluno',autenticarAdmin, controller.addStudent)
 
 module.exports = router
